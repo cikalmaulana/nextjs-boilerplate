f03e0c8aa384712f57095eff3231f87fb0302bb1
Cikal <rajasacikal.work@gmail.com>
2026-02-10 22:02:00 +0700

    Initial Commit

---
+++ src/app/dashboard/$element/client.dashboard.main.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function CE_DashboardMain(){
+    return (
+        <div className="h-screen flex items-center justify-center bg-black text-white text-4xl">
+            Tailwind OK
+        </div>
+    )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use server"
+
+import { CE_DashboardMain } from "./$element/client.dashboard.main"
+
+export default async function Page(){
+    return (
+        <CE_DashboardMain />
+    )
+}
